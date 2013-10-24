ca00142a9b1b8bb03c5a974d33c3c7d4161ec366
Danny Whalen <daniel.r.whalen@gmail.com>
2013-10-23 22:43:06 -0700

    Initial commit - generate app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PrivilegedSessions::Application.config.secret_key_base = 'ec403330a3d5d75fc69628cf255fad4ea4ceef8f5acba6bfcca5d8b02a741cacfae128f8b2ee2d2f02b4d44eb3420456a6445e7b6ea30e08e9f5f98e1a32a7a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PrivilegedSessions::Application.config.session_store :cookie_store, key: '_privileged_sessions_session'
