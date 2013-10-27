7ebc2efa9805e6f7be75956d9f3d1791ee332fee
Danny Whalen <daniel.r.whalen@gmail.com>
2013-10-26 22:15:49 -0700

    Load minimal javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require bootstrap/alert
+//= require bootstrap/collapse
+//= require bootstrap/dropdown
 //= require_tree .
